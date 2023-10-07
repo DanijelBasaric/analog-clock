21b8ecb0053972a1c6740b6bcd3b7ab4f426c4c1
DanijelBasaric <120591525+DanijelBasaric@users.noreply.github.com>
2023-10-07 13:23:09 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000);
+
+const secondsHand = document.querySelector('.secondsHand');
+const minutesHand = document.querySelector('.minutesHand');
+const hoursHand = document.querySelector('.hoursHand');
+
+function setClock () {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondsHand, secondsRatio);
+    setRotation(minutesHand, minutesRatio);
+    setRotation(hoursHand, hoursRatio);
+}
+
+const setRotation = (element, rotationRatio) => {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock();
